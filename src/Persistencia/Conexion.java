ea42c4a4bd55378ca7fd5889f82791784bcd6dc2
josemir01 <josemirzaleh45@gmail.com>
2025-09-25 21:00:25 -0300

    Initial commit

---
+++ src/Persistencia/Conexion.java
@@ -0,0 +1,34 @@
+
+package Persistencia;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+        
+
+public class Conexion {
+    private String url;
+    private String usuario;
+    private String password;
+    
+    private static Connection conexion=null;
+
+    public Conexion(String url, String usuario, String password) {
+        this.url = url;
+        this.usuario = usuario;
+        this.password = password;
+    }
+    public Connection BuscarConexion(){
+        if(conexion==null){
+            try{
+              Class.forName("org.mariadb.jdbc.Driver");
+              conexion=DriverManager.getConnection(url, usuario, password);
+            }catch(SQLException | ClassNotFoundException e){
+                JOptionPane.showMessageDialog(null, "no se pudo establecer la conexion");
+            }
+            
+        }
+        return conexion;
+        
+    }
+}
