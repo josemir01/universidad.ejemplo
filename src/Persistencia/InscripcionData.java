116b24de8b8c8610b2d795046deec02ea5956ffe
josemir01 <josemirzaleh45@gmail.com>
2025-10-07 20:38:18 -0300

    inscripcion,inscripcionData

--- src/Persistencia/InscripcionData.java
+++ src/Persistencia/InscripcionData.java
@@ -1,8 +1,12 @@
 package Persistencia;
 
+import Modelo.Alumno;
 import Modelo.Inscripcion;
+import Modelo.Materia;
 import java.sql.Connection;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InscripcionData {
     
@@ -36,8 +40,73 @@ public class InscripcionData {
           }else{
                 System.out.println("No se pudo obtener el ID ");
             }
+          
+           ps.close();
+           System.out.println("inscripcion guardada correctamente");
        }catch(SQLException ex){
            ex.printStackTrace();
        }
-   } 
+   }
+   
+   public void BorrarInscripcion(int id){
+       String sql = "DELETE * FROM inscripcion WHERE id_inscripto=?";
+       try{
+           
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
+                System.out.println("inscripcion borrada de la DB");
+            } else {
+                System.out.println("No se encuentra la inscripcion con ID " + id);
+            }
+            ps.close();   
+           
+       }catch(SQLException ex){
+          ex.printStackTrace();
+       }
+   }
+   
+  public void ActualizarNota(int id, int notanueva){
+      String sql = "UPDATE inscripcion SET nota=? WHERE id_inscripto=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, notanueva);
+            ps.setInt(2, id);
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
+                System.out.println("Nota actualizada correctamente");
+            } else {
+                System.out.println("No se encuentra la Nota con ID " + id);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+  }
+  
+  public ArrayList<Inscripcion> obtenerInscripciones(){
+      ArrayList<Inscripcion> inscriptos=new ArrayList<>();
+      String sql="Select * FROM inscripcion";
+      try{
+       PreparedStatement ps = con.prepareStatement(sql);
+       ResultSet rs = ps.executeQuery();
+       while(rs.next()){
+        int idInscripto=rs.getInt("id_inscripto");
+        int idAlumno=rs.getInt("id_alumno");
+        int idMateria=rs.getInt("id_materia");
+        int nota =rs.getInt("nota");
+        
+        Alumno alumno=idAlumno.buscarAlumno(idAlumno);
+        Materia materia=idMateria.buscarMateria(idMateria);
+        
+        Inscripcion inscripto=new Inscripcion();
+       }
+       
+      }catch(SQLException ex){
+        ex.printStackTrace();
+      }
+        return null;
+      
+  }
 }
